27d26bf10beb0e5d0605bb1dcf4d96195d0f2033
NateKonigkramer214 <nathankonigkramer85@gmail.com>
2023-11-06 17:03:14 +1300

    More comments

--- TaskApp/MVVM/Views/MainView.xaml.cs
+++ TaskApp/MVVM/Views/MainView.xaml.cs
@@ -1,42 +1,46 @@
-using TaskApp.MVVM.ViewModels;
+using TaskApp.MVVM.ViewModels; // Import the necessary ViewModel namespace
 
-namespace TaskApp.MVVM.Views;
-
-public partial class MainView : ContentPage
+namespace TaskApp.MVVM.Views
 {
-    private MainViewModel _mainViewModel = new MainViewModel();
-    public MainView()
-	{
-		InitializeComponent();
-        BindingContext = _mainViewModel;
-
-    }
-
-    private void checkBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    public partial class MainView : ContentPage
     {
-        _mainViewModel.UpdateData();
-    }
+        private MainViewModel _mainViewModel = new MainViewModel(); // Instantiate the MainViewModel
 
-    private void Adding_Clicked(object sender, EventArgs e)
-    {
-        var taskView = new NewTaskView()
+        // Constructor for the MainView
+        public MainView()
         {
-            BindingContext = new NewTaskViewModel
+            InitializeComponent(); // Initialize the components
+            BindingContext = _mainViewModel; // Set the binding context to the MainViewModel
+        }
+
+        // Event handler for the CheckBox's CheckedChanged event
+        private void checkBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            _mainViewModel.UpdateData(); // Call the UpdateData method in the MainViewModel
+        }
+
+        // Event handler for the button click to add a new task
+        private void Adding_Clicked(object sender, EventArgs e)
+        {
+            var taskView = new NewTaskView() // Create an instance of the NewTaskView
             {
-                Tasks = _mainViewModel.Tasks,
-                Categories = _mainViewModel.Categories
+                BindingContext = new NewTaskViewModel // Set the binding context to a new instance of NewTaskViewModel
+                {
+                    // Pass the Tasks and Categories from the MainViewModel to the NewTaskViewModel
+                    Tasks = _mainViewModel.Tasks,
+                    Categories = _mainViewModel.Categories
+                }
+            };
+
+            // Check if the taskView was successfully created
+            if (taskView != null)
+            {
+                Navigation.PushAsync(taskView); // Navigate to the NewTaskView
             }
-        };
-
-        if (taskView != null )
-        {
-            Navigation.PushAsync(taskView);
+            else
+            {
+                DisplayAlert("ERROR", "Loading page", "Ok"); // Show an error alert if the page failed to load
+            }
         }
-        else
-        {
-            DisplayAlert("ERROR", "Loading page", "Ok");
-        }
-
-        
     }
 }
--- TaskApp/MVVM/Views/NewTaskView.xaml.cs
+++ TaskApp/MVVM/Views/NewTaskView.xaml.cs
@@ -1,8 +1,7 @@
-using TaskApp.MVVM.ViewModels;
-using TaskApp.MVVM.Models;
-using System.Linq;
-using System;
-
+using TaskApp.MVVM.ViewModels; // Import ViewModel namespace
+using TaskApp.MVVM.Models; // Import Models namespace
+using System.Linq; // Import LINQ functionalities
+using System; // Import System namespace
 
 namespace TaskApp.MVVM.Views
 {
@@ -10,16 +9,20 @@ namespace TaskApp.MVVM.Views
     {
         public NewTaskView()
         {
-            InitializeComponent();
+            InitializeComponent(); // Initialize the NewTaskView
         }
 
+        // Method for handling 'Add Task' button click event
         private async void btnAddTask_Clicked(object sender, EventArgs e)
         {
-            var vm = BindingContext as NewTaskViewModel;
+            var vm = BindingContext as NewTaskViewModel; // Get the binding context as NewTaskViewModel
+
+            // Get the selected category from the view model
             var selectedCategory = vm.Categories.Where(x => x.IsSelected).FirstOrDefault();
 
             if (selectedCategory != null)
             {
+                // Create a new task with details from the view model
                 var task = new MyTask
                 {
                     TaskName = vm.Task,
@@ -27,41 +30,45 @@ namespace TaskApp.MVVM.Views
                     TaskColor = selectedCategory.Color
                 };
 
-                vm.Tasks.Add(task);
+                vm.Tasks.Add(task); // Add the new task to the tasks list in the view model
 
-                // Manually call UpdateData from MainViewModel
+                // Manually trigger the UpdateData method in MainViewModel
                 var mainViewModel = App.Current.MainPage.BindingContext as MainViewModel;
-                mainViewModel.UpdateData();
+                mainViewModel.UpdateData(); // Update the data
 
-                await Navigation.PopAsync();
+                await Navigation.PopAsync(); // Navigate back to the previous page
             }
             else
             {
+                // Show an alert if no category is selected
                 await DisplayAlert("Invalid Selection", "You must select a category", "OK");
             }
         }
 
+        // Method for handling 'Add Category' button click event
         private async void btnAddCategory_Clicked(object sender, EventArgs e)
         {
-            var vm = BindingContext as NewTaskViewModel;
+            var vm = BindingContext as NewTaskViewModel; // Get the binding context as NewTaskViewModel
 
+            // Prompt the user to enter a new category name
             string category = await DisplayPromptAsync("New Category", "Write the category name", maxLength: 50, keyboard: Keyboard.Text);
 
             if (!string.IsNullOrEmpty(category))
             {
                 var random = new Random(); // Create a Random instance
 
+                // Create a new category with a randomly generated color and provided name
                 var newCategory = new Category
                 {
                     Id = vm.Categories.Max(x => x.Id) + 1,
-                    Color = Color.FromRgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255)).ToHex(), // Use 'random' to generate colors
+                    Color = Color.FromRgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255)).ToHex(),
                     CategoryName = category
                 };
-                vm.Categories.Add(newCategory);
+                vm.Categories.Add(newCategory); // Add the new category to the categories list in the view model
 
-                // Manually call UpdateData from MainViewModel
+                // Manually trigger the UpdateData method in MainViewModel
                 var mainViewModel = App.Current.MainPage.BindingContext as MainViewModel;
-                mainViewModel.UpdateData();
+                mainViewModel.UpdateData(); // Update the data
             }
         }
     }
